ab9180e65c82a551a1741857cb75cf592f96192a
Irina M. S <irinamardhatillah@gmail.com>
2023-12-27 17:47:08 +0700

    Implement batch processing

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import matplotlib
 import numpy as np
 
-inputs = [1.0, 2.0, 3.0, 2.5]
+inputs = [
+    [1.0, 2.0, 3.0, 2.5],
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8]
+]
 
 weights = [
     [0.2, 0.8, -0.5, 1.0],
@@ -11,6 +15,6 @@ weights = [
 
 biases = [2, 3, 0.5]
 
-layer_outputs = np.dot(weights, inputs) + biases
+layer_outputs = np.dot(inputs, np.array(weights).T) + biases
 
 print(layer_outputs)
