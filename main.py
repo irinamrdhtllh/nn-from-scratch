b06fac44bdfdc319a3d8048671a50eeca156fa86
Irina M. S <irinamardhatillah@gmail.com>
2023-12-29 15:31:54 +0700

    Implement categorical cross entropy loss function

--- main.py
+++ main.py
@@ -26,18 +26,44 @@ class ActivationSoftmax:
         self.output = probabilities
 
 
-X, y = spiral_data(samples=100, classes=3)
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
 
-dense1 = LayerDense(2, 16)
-activation1 = ActivationReLU()
 
-dense2 = LayerDense(16, 3)
-activation2 = ActivationSoftmax()
+class LossCategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
 
-dense1.forward(X)
-activation1.forward(dense1.output)
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
 
-dense2.forward(activation1.output)
-activation2.forward(dense2.output)
+        negative_log_likelihoods = -np.log(correct_confidences)
 
-print(activation2.output[:5])
+        return negative_log_likelihoods
+
+
+if __name__ == "__main__":
+    X, y = spiral_data(samples=100, classes=3)
+
+    dense1 = LayerDense(2, 16)
+    activation1 = ActivationReLU()
+
+    dense2 = LayerDense(16, 3)
+    activation2 = ActivationSoftmax()
+
+    dense1.forward(X)
+    activation1.forward(dense1.output)
+
+    dense2.forward(activation1.output)
+    activation2.forward(dense2.output)
+
+    loss_function = LossCategoricalCrossEntropy()
+    loss = loss_function.calculate(activation2.output, y)
+
+    print("Loss:", loss)
