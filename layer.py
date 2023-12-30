4b9f0b3de9ddfccf1884ac9f540d693f0daefff1
Irina M. S <irinamardhatillah@gmail.com>
2023-12-30 22:42:06 +0700

    Implement backward pass for dense layer

--- layer.py
+++ layer.py
@@ -4,9 +4,17 @@ import numpy as np
 class Dense:
     def __init__(self, n_inputs, n_neurons):
         # Initialize weights and biases
-        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
 
     def forward(self, inputs):
         # Calculate output values from inputs, weights, and biases
-        self.output = np.dot(inputs, self.weights + self.biases)
+        self.inputs = inputs
+        self.output = np.dot(self.inputs, self.weights) + self.biases
+
+    def backward(self, dvalues):
+        # Gradients on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        # Gradient on values
+        self.dinputs = np.dot(dvalues, self.weights.T)
