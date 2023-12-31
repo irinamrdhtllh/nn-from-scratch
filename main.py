f7ce6598c18af20522f1e7aefbae9ddcf27d2c28
Irina M. S <irinamardhatillah@gmail.com>
2023-12-31 14:37:14 +0700

    Update main with the latest trial code

--- main.py
+++ main.py
@@ -2,76 +2,57 @@ import numpy as np
 import nnfs
 from nnfs.datasets import spiral_data
 
-from activation import ReLU, Softmax
+from activation import ReLU
 from layer import Dense
-from loss import CategoricalCrossEntropy
+from loss import Softmax_CategoricalCrossEntropy
+from optimizer import SGD
 
 nnfs.init()
 
 
-def main():
-    # Create the dataset
+def trial():
     X, y = spiral_data(samples=100, classes=3)
 
-    # Create the model
-    dense1 = Dense(2, 16)
+    # Create the 1st Dense layer with 2 input features (x, y) and 128 output values
+    dense1 = Dense(2, 128)
+    # Create ReLU activation for the 1st layer
     activation1 = ReLU()
-    dense2 = Dense(16, 3)
-    activation2 = Softmax()
+    # Create the 2nd Dense layer with 128 input features and 3 output values (same as the number of the data classes)
+    dense2 = Dense(128, 3)
 
-    # Create the loss function
-    loss_function = CategoricalCrossEntropy()
+    # Create Softmax classifier's combined activation and loss
+    activation_loss = Softmax_CategoricalCrossEntropy()
 
-    # Helper variables
-    lowest_loss = 1e6
-    best_dense1_weights = dense1.weights.copy()
-    best_dense1_biases = dense1.biases.copy()
-    best_dense2_weights = dense2.weights.copy()
-    best_dense2_biases = dense2.biases.copy()
+    # Create optimizer
+    optimizer = SGD()
 
-    for iteration in range(10_000):
-        # Update weights and biases with small random values
-        dense1.weights += 0.05 * np.random.randn(2, 16)
-        dense1.biases += 0.05 * np.random.randn(1, 16)
-        dense2.weights += 0.05 * np.random.randn(16, 3)
-        dense2.biases += 0.05 * np.random.randn(1, 3)
-
-        # Perform a forward pass of the training data through layers
+    for epoch in range(10_000 + 1):
+        # Perform a forward pass
         dense1.forward(X)
         activation1.forward(dense1.output)
         dense2.forward(activation1.output)
-        activation2.forward(dense2.output)
 
-        # Calculate the loss
-        loss = loss_function.calculate(activation2.output, y)
+        loss = activation_loss.forward(dense2.output, y)
 
-        # Calculate the accuracy
-        predictions = np.argmax(activation2.output, axis=1)
+        # Calculate accuracy from activation2's output and target
+        predictions = np.argmax(activation_loss.output, axis=1)
+        if len(y.shape) == 2:
+            y = np.argmax(y, axis=1)
         accuracy = np.mean(predictions == y)
 
-        # If loss is smaller, print and save weights and biases aside
-        if loss < lowest_loss:
-            print(
-                "Weights and biases updated,",
-                "iteration:",
-                iteration,
-                "loss:",
-                loss,
-                "accuracy:",
-                accuracy,
-            )
-            best_dense1_weights = dense1.weights.copy()
-            best_dense1_biases = dense1.biases.copy()
-            best_dense2_weights = dense2.weights.copy()
-            best_dense2_biases = dense2.biases.copy()
-            lowest_loss = loss
-        # Revert weight and biases
-        else:
-            dense1.weights = best_dense1_weights.copy()
-            dense1.biases = best_dense1_biases.copy()
-            dense2.weights = best_dense2_weights.copy()
-            dense2.biases = best_dense2_biases.copy()
+        if not epoch % 100:
+            print(f"epoch: {epoch}, " + f"acc: {accuracy:.3f}, " + f"loss: {loss:.3f}")
+
+        # Backward pass
+        activation_loss.backward(activation_loss.output, y)
+        dense2.backward(activation_loss.dinputs)
+        activation1.backward(dense2.dinputs)
+        dense1.backward(activation1.dinputs)
+
+        # Update wweights and biases
+        optimizer.update_params(dense1)
+        optimizer.update_params(dense2)
 
 
 if __name__ == "__main__":
-    main()
+    trial()
