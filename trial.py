d8132bd733ea62cff3d0315e34992e65bc790f14
Irina M. S <irinamardhatillah@gmail.com>
2024-01-01 14:57:35 +0700

    Implement learning rate decay for SGD optimizer

--- trial.py
+++ trial.py
@@ -14,17 +14,17 @@ def trial():
     X, y = spiral_data(samples=100, classes=3)
 
     # Create the 1st Dense layer with 2 input features (x, y) and 128 output values
-    dense1 = Dense(2, 128)
+    dense1 = Dense(2, 64)
     # Create ReLU activation for the 1st layer
     activation1 = ReLU()
     # Create the 2nd Dense layer with 128 input features and 3 output values (same as the number of the data classes)
-    dense2 = Dense(128, 3)
+    dense2 = Dense(64, 3)
 
     # Create Softmax classifier's combined activation and loss
     activation_loss = Softmax_CategoricalCrossEntropy()
 
     # Create optimizer
-    optimizer = SGD()
+    optimizer = SGD(decay=1e-3)
 
     for epoch in range(10_000 + 1):
         # Perform a forward pass
@@ -41,7 +41,12 @@ def trial():
         accuracy = np.mean(predictions == y)
 
         if not epoch % 100:
-            print(f"epoch: {epoch}, " + f"acc: {accuracy:.3f}, " + f"loss: {loss:.3f}")
+            print(
+                f"epoch: {epoch}, "
+                + f"acc: {accuracy:.3f}, "
+                + f"loss: {loss:.3f}, "
+                + f"lr: {optimizer.current_learning_rate}"
+            )
 
         # Backward pass
         activation_loss.backward(activation_loss.output, y)
@@ -50,8 +55,10 @@ def trial():
         dense1.backward(activation1.dinputs)
 
         # Update wweights and biases
+        optimizer.pre_update_params()
         optimizer.update_params(dense1)
         optimizer.update_params(dense2)
+        optimizer.post_update_params()
 
 
 if __name__ == "__main__":
