cf5c2364cf2fafec3ffb313cf831228fe8954a1f
Irina M. S <irinamardhatillah@gmail.com>
2024-01-03 22:59:37 +0700

    Organize the code into train and test functions

--- trial.py
+++ trial.py
@@ -5,35 +5,20 @@ from nnfs.datasets import spiral_data
 from modules.activation import ReLU
 from modules.layer import Dense
 from modules.loss import Softmax_CategoricalCrossEntropy
-from modules.optimizer import SGD, AdaGrad, RMSProp, Adam
+from modules.optimizer import Adam
 
 nnfs.init()
 
 
-def trial():
-    X, y = spiral_data(samples=1_000, classes=3)
-
-    # Create the 1st Dense layer with 2 input features (x, y) and 256 output values
-    dense1 = Dense(2, 512, weight_lambda_l2=5e-4, bias_lambda_l2=5e-4)
-    # Create ReLU activation for the 1st layer
-    activation1 = ReLU()
-    # Create the 2nd Dense layer with 256 input features and 3 output values (same as the number of the data classes)
-    dense2 = Dense(512, 3)
-
-    # Create Softmax classifier's combined activation and loss
-    activation_loss = Softmax_CategoricalCrossEntropy()
-
-    # Create optimizer
-    optimizer = Adam(learning_rate=0.05, decay=5e-7)
-
+def train(X_train, y_train):
     # Train the model
-    for epoch in range(10_000 + 1):
+    for epoch in range(10000 + 1):
         # Perform a forward pass
-        dense1.forward(X)
+        dense1.forward(X_train)
         activation1.forward(dense1.output)
         dense2.forward(activation1.output)
 
-        data_loss = activation_loss.forward(dense2.output, y)
+        data_loss = activation_loss.forward(dense2.output, y_train)
 
         # fmt: off
         regularization_loss = (
@@ -46,9 +31,9 @@ def trial():
 
         # Calculate accuracy from activation2's output and target
         predictions = np.argmax(activation_loss.output, axis=1)
-        if len(y.shape) == 2:
-            y = np.argmax(y, axis=1)
-        accuracy = np.mean(predictions == y)
+        if len(y_train.shape) == 2:
+            y_train = np.argmax(y_train, axis=1)
+        accuracy = np.mean(predictions == y_train)
 
         if not epoch % 100:
             print(
@@ -61,7 +46,7 @@ def trial():
             )
 
         # Backward pass
-        activation_loss.backward(activation_loss.output, y)
+        activation_loss.backward(activation_loss.output, y_train)
         dense2.backward(activation_loss.dinputs)
         activation1.backward(dense2.dinputs)
         dense1.backward(activation1.dinputs)
@@ -72,9 +57,8 @@ def trial():
         optimizer.update_params(dense2)
         optimizer.post_update_params()
 
-    # Validate the model
-    X_test, y_test = spiral_data(samples=100, classes=3)
 
+def test(X_test, y_test):
     dense1.forward(X_test)
     activation1.forward(dense1.output)
     dense2.forward(activation1.output)
@@ -90,4 +74,19 @@ def trial():
 
 
 if __name__ == "__main__":
-    trial()
+    # Create the dataset
+    X_train, y_train = spiral_data(samples=1000, classes=3)
+    X_test, y_test = spiral_data(samples=100, classes=3)
+
+    # Create the model
+    dense1 = Dense(2, 512, weight_lambda_l2=5e-4, bias_lambda_l2=5e-4)
+    activation1 = ReLU()
+    dense2 = Dense(512, 3)
+    activation_loss = Softmax_CategoricalCrossEntropy()
+    optimizer = Adam(learning_rate=0.02, decay=5e-7)
+
+    # Train the model
+    train(X_train, y_train)
+
+    # Validate the model
+    test(X_test, y_test)
