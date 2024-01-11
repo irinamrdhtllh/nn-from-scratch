5f1a43d15dde0080ff5b98cf3c719816cbb18105
Irina M. S <irinamardhatillah@gmail.com>
2024-01-11 19:28:22 +0700

    Modify the path to save the fashion-mnist dataset

--- examples/fashion_mnist/data_retrieving.py
+++ examples/fashion_mnist/data_retrieving.py
@@ -6,8 +6,8 @@ from zipfile import ZipFile
 
 # The dataset URL
 URL = "https://nnfs.io/datasets/fashion_mnist_images.zip"
-FILE = "examples/fashion_mnist/fashion_mnist_images.zip"
-FOLDER = "examples/fashion_mnist/fashion_mnist_images"
+FILE = "examples/fashion_mnist/images.zip"
+FOLDER = "examples/fashion_mnist/images"
 
 if not os.path.isfile(FILE):
     print(f"Downloading {URL} and saving as {FILE}...")
